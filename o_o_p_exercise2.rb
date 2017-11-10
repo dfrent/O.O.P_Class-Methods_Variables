691c07159039e8264e357c524bbe69ea91822e62
cordell <cordell.perry@gmail.com>
2017-11-10 12:20:05 -0500

    bank account oop2

--- o_o_p_exercise2.rb
+++ o_o_p_exercise2.rb
@@ -1,24 +1,72 @@
 class Bank_account
 
+  @@interest_rate = 1.5
+  @@accounts = []
+  @@total_funds = 0
 
+  def initialize
+    @balance = 0
 
-def initialize
+  end
+
+  ##----READERS----
+  def balance
+    @balance
+  end
+
+  ##----WRITERS----
+  def balance=(balance)
+    @balance = balance
+  end
+
+  ##----INSTANCE----
+
+  def deposit(money)
+   @balance = (@balance + money)
+  end
+
+  def withdraw(taking)
+    @balance = (@balance - taking)
+  end
+
+  ##-----CLASS-----
+  def self.create
+     new_bank_acount = Bank_account.new
+     @@accounts << new_bank_acount
+     return new_bank_acount
+   end
+
+   def self.total_funds
+      @@accounts.map do |account|
+      @@total_funds += account.balance
+      end
+      return @@total_funds
+   end
+
+  def self.interest_time
+      @@accounts.map do |account|
+        account.change_balance(account.account_balance * @@interest_rate)
+      end
+    end
 
 end
 
-#----READERS----
+my_account = Bank_account.create
 
+puts my_account.balance
+my_account.deposit(350)
+puts my_account.balance
+my_account.withdraw(150)
+puts my_account.balance
 
+your_account = Bank_account.create
 
-#----WRITERS----
+puts my_account.balance
+puts Bank_account.total_funds
 
+my_account.deposit(200)
+your_account.deposit(1000)
 
-
-
-#----INSTANCE----
-
-
-
-
-
-end
+puts my_account.balance.inspect
+puts Bank_account.total_funds
+# puts your_account.balance
