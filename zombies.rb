d27b6dc9a243202c3700a6a5f0ec6bf1227bb45d
cordell <cordell.perry@gmail.com>
2017-11-12 19:24:37 -0500

    zombies exercise skeleton

---
+++ zombies.rb
@@ -0,0 +1,24 @@
+class Zombie
+
+@@horde = [] #contains collection of all zombies
+@@plauge_level = 10 #used to determing rate of zombie spawn
+@@max_speed = 5 #max value for zombies unchanging
+@@max_strength = 8 # max value for zombie strength unchanging
+@@defualt_speed = 1 # unchanging value
+@@defualt_strength = 3 #unchanging value
+
+
+def initialize(spawn_speed, spawn_strengh)
+
+end
+
+##----READERS----
+
+##----WRITERS----
+
+##----INSTANCE----
+
+##----CLASS----
+
+
+end
